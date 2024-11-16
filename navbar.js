f2a4e40182a36b2c49a4e49c9181fa08e323cb6d
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2024-11-16 22:29:03 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            Social Media Mini-App
+        </div>
+    );
+};
+
+export default Navbar;
