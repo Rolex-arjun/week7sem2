c0f0096ee6f7a961b53d0a7e563585da4a03f5b0
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2024-11-16 19:33:43 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { login } from '../services/api';
+
+const Login = () => {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login(form);
+      alert('Logged in successfully');
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="email" type="email" onChange={handleChange} placeholder="Email" />
+      <input name="password" type="password" onChange={handleChange} placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
